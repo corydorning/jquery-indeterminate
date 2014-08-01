487f33dd958b9662271baa9d56ecaad453347bbe
Cory Dorning <cory@dorning.me>
2014-08-01 14:04:55 -0400

    Set state on init for checked checkboxes

--- jquery-indeterminate.js
+++ jquery-indeterminate.js
@@ -23,8 +23,7 @@
   $.fn.indeterminate = function(options) {
         // set defaults
     var defaults = {
-          label: false, // checkbox contained within label (true/false)
-          state: false // default state (true/false)
+          label: false // checkbox contained within label (true/false)
         },
 
         // overwrite default options with those set
@@ -39,96 +38,105 @@
       var $set = $(this),
 
           // check for label container
-          label = $set.data('label') || settings.label;
+          label = $set.data('label') || settings.label,
+
+          setState = function() {
+            // checked?
+            var $this = $(this),
+
+            checked = $this.prop('checked'),
+
+            // get parent container
+            $parent = $this.closest('li'),
+
+            // check if checkbox parent is a part of the set
+            setCheck = function($el) {
+              // if element parent
+              if (!$set.is($el.parent())) {
+                // check parents siblings
+                siblingCheck($el);
+              }
+
+            },
+
+            // recursive sibling check
+            siblingCheck = function($el) {
+              // current checkbox parent
+              var $currParent = $el.parent().parent(),
+
+              // check for label and get current child checkboxes
+                $currChildren = label ? $currParent.children('label').children(':checkbox') : $currParent.children(':checkbox'),
+
+              // assume siblings match current checkbox
+                matched = true;
+
+              // check if sibling checkboxes match
+              $el.siblings().each(function() {
+                // check for label and get sibling child checkboxes
+                var $sibling = label ? $(this).children('label') : $(this);
+
+                // update match variable
+                matched = $sibling.children(':checkbox').prop('checked') === checked;
+
+                // keep going if they match
+                return matched;
+
+              }); // end loop
+
+              if (matched && checked) { // if siblings and checkbox are checked
+
+                // set parent to checked
+                $currChildren.prop({
+                  checked: checked,
+                  indeterminate: false
+                });
+
+                // check if current parent is contained within the set
+                setCheck($currParent);
+
+              } else if (matched && !checked) { // if siblings and checkbox are unchecked
+
+                // set parent to unchecked
+                $currChildren.prop({
+                  checked: checked,
+                  indeterminate: ($currParent.children('ul').find(':checked').length > 0)
+                });
+
+                // check if current parent is contained within the set
+                setCheck($currParent);
+
+              } else { // some checked/unchecked
+                // check for label and get checkbox ancestors within the set
+                var $ancestors = label ? $el.parentsUntil($set).children('label') : $el.parentsUntil($set);
+
+                // set ancestors to indeterminate
+                $ancestors.children(':checkbox').prop({
+                  checked: false,
+                  indeterminate: true
+                });
+
+              } // end if matched && checked
+
+            }; // end siblingCheck
+
+            // set all child checkboxes to match parent
+            $parent.find(':checkbox').prop({
+              checked: checked,
+              indeterminate: false
+            });
+            console.log(this);
+            // check if current parent is contained within the set
+            setCheck($parent);
+          }; // setState
 
 
-        // add change event to checkboxes within the set
-        $set.find(':checkbox').change(function(e) {
-          // checked?
-          var checked = $(this).prop('checked'),
-
-              // get parent container
-              $parent = $(this).closest('li'),
-
-              // check if checkbox parent is a part of the set
-              setCheck = function($el) {
-                // if element parent
-                if (!$set.is($el.parent())) {
-                  // check parents siblings
-                  siblingCheck($el);
-                }
-
-              },
-
-              // recursive sibling check
-              siblingCheck = function($el) {
-                // current checkbox parent
-                var $currParent = $el.parent().parent(),
-
-                    // check for label and get current child checkboxes
-                    $currChildren = label ? $currParent.children('label').children(':checkbox') : $currParent.children(':checkbox'),
-
-                    // assume siblings match current checkbox
-                    matched = true;
-
-                // check if sibling checkboxes match
-                $el.siblings().each(function() {
-                      // check for label and get sibling child checkboxes
-                  var $sibling = label ? $(this).children('label') : $(this);
-
-                  // update match variable
-                  matched = $sibling.children(':checkbox').prop('checked') === checked;
-
-                  // keep going if they match
-                  return matched;
-
-                }); // end loop
-
-                if (matched && checked) { // if siblings and checkbox are checked
-
-                  // set parent to checked
-                  $currChildren.prop({
-                    checked: checked,
-                    indeterminate: false
-                  });
-
-                  // check if current parent is contained within the set
-                  setCheck($currParent);
-
-                } else if (matched && !checked) { // if siblings and checkbox are unchecked
-
-                  // set parent to unchecked
-                  $currChildren.prop({
-                    checked: checked,
-                    indeterminate: ($currParent.children('ul').find(':checked').length > 0)
-                  });
-
-                  // check if current parent is contained within the set
-                  setCheck($currParent);
-
-                } else { // some checked/unchecked
-                      // check for label and get checkbox ancestors within the set
-                  var $ancestors = label ? $el.parentsUntil($set).children('label') : $el.parentsUntil($set);
-
-                  // set ancestors to indeterminate
-                  $ancestors.children(':checkbox').prop({
-                    checked: false,
-                    indeterminate: true
-                  });
-
-                } // end if matched && checked
-
-              }; // end siblingCheck
-
-          // set all child checkboxes to match parent
-          $parent.find(':checkbox').prop({
-            checked: checked,
-            indeterminate: false
-          });
-
-          // check if current parent is contained within the set
-          setCheck($parent);
-        });
+        $set
+          .find(':checkbox')
+            // add change event to checkboxes within the set
+            .change(setState)
+          // get checked on init, trigger change event to set state
+          .filter(':checked')
+            .each(function() { $(this).trigger('change'); });
     });
 
   };
